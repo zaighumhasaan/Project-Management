34595e90f1bd5631f44d10d7e05ae9e9a64400b5
zaighumrajput82 <152955864+zaighumrajput82@users.noreply.github.com>
2025-02-16 01:11:48 +0000

    project model created

---
+++ projects/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Many-to-Many Relationship: A project can have multiple users
+    users = models.ManyToManyField(User, related_name='projects', blank=True)
+
+    def __str__(self):
+        return self.title
